760ea8ac6684e654b3d1df1caa1c063191b2b64c
Eduardo Coelho <eduardocoelho619@gmail.com>
2024-02-07 20:59:10 -0300

    create: creating new rotes

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,19 @@
+import z from "zod"
+import { FastifyInstance } from "fastify"
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post('/polls/:pollId/votes', async (request, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid()
+    })
+
+    const voteOnPollParams = z.object({
+      pollId:  z.string().uuid()
+    })
+  
+    const { pollId } = voteOnPollParams.parse(request.body)
+    const { pollOptionId } = voteOnPollBody.parse(request.body)
+  
+    return reply.status(201).send()
+  })
+}
